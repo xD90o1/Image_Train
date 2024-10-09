06b06919ac0888c2523ceaf1840165e41c54ec10
xD90o1 <aceervite@gmail.com>
2024-10-09 19:21:58 +0800

    new file:   .gitattributes 	new file:   .idea/.gitignore 	new file:   .idea/IMAGE.py.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   labels.txt 	new file:   model.keras 	new file:   model.tflite 	renamed:    convert_to_tflite.py -> tflite_converter.py 	modified:   train_model.py

--- train_model.py
+++ train_model.py
@@ -5,10 +5,10 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 import subprocess
 import os
 
-# Set paths
-base_dir = 'dataset'  # Ensure your dataset folder is structured properly
 
-# Data preprocessing with augmentation
+base_dir = 'dataset'
+
+
 datagen = ImageDataGenerator(
     rescale=1./255,
     validation_split=0.2,
@@ -36,14 +36,13 @@ validation_generator = datagen.flow_from_directory(
     subset='validation'
 )
 
-# Save class labels to labels.txt
 labels_file_path = 'labels.txt'
 with open(labels_file_path, 'w') as f:
     for label, index in train_generator.class_indices.items():
         f.write(f"{label}\n")
 print(f"Labels saved to {labels_file_path}")
 
-# Build the model
+#CNN Algorithm
 model = Sequential()
 model.add(Input(shape=(224, 224, 3)))  # Correctly add the Input layer
 model.add(Conv2D(32, (3, 3), activation='relu'))
@@ -52,21 +51,21 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(len(train_generator.class_indices), activation='softmax'))
 
-# Compile the model
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
+
 model.fit(train_generator, validation_data=validation_generator, epochs=10)
 
-# Save the model in Keras format
-model.save('my_model.keras')
+
+model.save('model.keras')
 
 # Run the convert_to_tflite.py script to convert the model to TensorFlow Lite
 print("Converting the Keras model to TensorFlow Lite...")
-subprocess.run(['python', 'convert_to_tflite.py'], check=True)
+subprocess.run(['python', 'tflite_converter.py'], check=True)
 
-# Show where the .tflite file is saved
-tflite_file_path = os.path.join(os.getcwd(), 'my_model.tflite')
+
+tflite_file_path = os.path.join(os.getcwd(), 'model.tflite')
 if os.path.exists(tflite_file_path):
     print(f"TensorFlow Lite model has been generated at: {tflite_file_path}")
 else:
